bd98d988551f39b78d4bd2d42d113f7b88567458
Sandia <firensonic@sandia-gaming.se>
2015-01-23 08:48:33 +0100

    Spelar classen är här börja coda

---
+++ spelare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+namespace Trollkarlskriget
+{
+	public class spelare
+	{
+		public spelare ()
+		{
+		}
+	}
+}
+

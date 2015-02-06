82d9c8c9b999bdc604e977d0ebe875f1b561afc8
Albin Forsberg <albin.forsberg@falun.ntig.se>
2015-02-06 09:40:40 +0100

    Snabbfix klasser

--- TrollkarlKriget/TrollkarlKriget/Classes/dragon.cs
+++ TrollkarlKriget/TrollkarlKriget/Classes/dragon.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Wizards.Classes
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Wizards
 {
-    class dragon
+    class dragon : enemy
     {
+        public dragon(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        { }
     }
 }
--- TrollkarlKriget/TrollkarlKriget/Classes/enemy.cs
+++ TrollkarlKriget/TrollkarlKriget/Classes/enemy.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Wizards.Classes
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Wizards
 {
-    class enemy
+    class enemy : physobj
     {
+        public enemy(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        { }
+
     }
 }

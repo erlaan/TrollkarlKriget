8c748f144f77ae46cad759aacc30424adc682158
Sandia <firensonic@sandia-gaming.se>
2015-02-06 11:11:13 +0100

    Hej hej här kommer det grejer.

--- TrollkarlKriget/TrollkarlKriget/Game1.cs
+++ TrollkarlKriget/TrollkarlKriget/Game1.cs
@@ -24,7 +24,7 @@ namespace Wizards
 		SpriteBatch spriteBatch;
         public player p1; // Player 1
         World world; //The map
-        Camera cam;
+        public Camera cam;
 
 		public Game1 ()
 		{
@@ -51,13 +51,14 @@ namespace Wizards
 			spriteBatch = new SpriteBatch (GraphicsDevice);
 
             p1 = new player(Content.Load<Texture2D>("images/players/sprite"),
-            new Vector2(0, 0),
+            new Vector2(0, 100),
             Keys.W, Keys.D, Keys.A, Keys.R, Keys.Space);
             Texture2D tile_texture = Content.Load<Texture2D>("images/world/square");
 
             cam = new Camera();
             world = new World(tile_texture);
             world.firesprite = Content.Load<Texture2D>("images/flamesprite");
+            world.cam = cam; 
 		}
 			
 		protected override void Update (GameTime gameTime)

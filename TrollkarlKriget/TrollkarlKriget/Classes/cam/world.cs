639794a431b9f86aced85be5c4608947c30aecca
Sandia <firensonic@sandia-gaming.se>
2015-02-13 09:51:27 +0100

    Laggt till ett block i mapen som vi ska vara portal till level 2

--- TrollkarlKriget/TrollkarlKriget/Classes/cam/world.cs
+++ TrollkarlKriget/TrollkarlKriget/Classes/cam/world.cs
@@ -63,8 +63,14 @@ namespace Wizards
                     }
                     else if (myColor == System.Drawing.Color.FromArgb(0, 0, 255))
                     {
+                        //Enemy Underground boss.
                         map[x, y] = new Tile(1, new Vector2(x * (texture.Width / numberOfTilesInTexture), y * (texture.Height)), texture);
                         
+                    }
+                    else if (myColor == System.Drawing.Color.FromArgb(255, 0, 255))
+                    {
+                        //Portal
+                        map[x, y] = new Tile(1, new Vector2(x * (texture.Width / numberOfTilesInTexture), y * (texture.Height)), texture);
                     }
                     else
                     {

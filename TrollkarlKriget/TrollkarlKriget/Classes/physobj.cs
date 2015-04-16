9a7493ad22397a70143604f05ff9070f97e75bc2
Jonathan Bergfeldt <jonathan.bergfeldt@gmail.com>
2015-04-16 14:10:36 +0200

    allalala kollision

--- TrollkarlKriget/TrollkarlKriget/Classes/physobj.cs
+++ TrollkarlKriget/TrollkarlKriget/Classes/physobj.cs
@@ -24,7 +24,8 @@ namespace Wizards
         public Vector2 curSpeed = new Vector2(0, 0);
         float speed;
 
-        public physobj(Texture2D texture, Vector2 position, Vector2 speed) : base (texture, position)
+        public physobj(Texture2D texture, Vector2 position, Vector2 speed)
+            : base(texture, position)
         {
             this.curSpeed = speed;
             this.position = position;
@@ -37,12 +38,12 @@ namespace Wizards
                 Convert.ToInt32(position.X),
                 Convert.ToInt32(position.Y),
                 Convert.ToInt32(Width),
-                Convert.ToInt32(Height/3));
+                Convert.ToInt32(Height / 3));
             Rectangle otherRect = new Rectangle(
                 Convert.ToInt32(other.position.X),
                 Convert.ToInt32(other.position.Y),
                 Convert.ToInt32(other.Width),
-                Convert.ToInt32(other.Height/3));
+                Convert.ToInt32(other.Height / 3));
             return myRect.Intersects(otherRect);
         }
 
@@ -51,7 +52,37 @@ namespace Wizards
 
         public void checkCollision(Camera cam, World world)
         {
-            
+            Rectangle playerRect = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height/3);
+            foreach (Tile tile in cam.visibleTiles) 
+            {
+                Rectangle tileRect = new Rectangle((int)tile.position.X, (int)tile.position.Y, Settings.gridsize, Settings.gridsize);
+                if (tile.type!=0 && playerRect.Intersects(tileRect)){
+                    switch(Math.Abs(position.X-tile.position.X)>Math.Abs(position.Y-tile.position.Y)){
+                        case (true):
+                            if (curSpeed.Y > 0)
+                            {
+                                position.Y -= position.Y - tile.position.Y;
+                            }
+                            else
+                            {
+                                position.Y += position.Y - tile.position.Y;
+                            }
+                            break;
+                        case (false):
+                            if (curSpeed.X < 0)
+                            {
+                                position.X += position.X - tile.position.X;
+                            }
+                            else
+                            {
+                                position.X -= position.X - tile.position.X;
+                            }
+                            break;
+                    }
+                playerRect = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height/3);
+
+                }
+            }
             if (inAir && action == Actions.Still)
             {
                 action = Actions.Jump;
@@ -59,36 +90,32 @@ namespace Wizards
             inAir = true;
 
             //Check collision
-            int posX = (int)position.X/Settings.gridsize;
-            int posY = (int)position.Y/Settings.gridsize;
-            for (int x = Math.Max(0, posX-1); x < Math.Max(0, (int)Math.Ceiling((double)(posX + 2))); x++)
+            /*int posX = (int)(position.X / Settings.gridsize);
+            int posY = (int)(position.Y / Settings.gridsize);
+            for (int x = Math.Max(0, posX); x < Math.Min(world.worldSize, (int)Math.Ceiling((double)(posX + 2))); x++)
             {
-                for (int y = Math.Max(0, posY); y < Math.Max(0, posY+3); y++)
+                for (int y = Math.Max(0, posY); y < Math.Min(world.worldSize, posY + 3); y++)
                 {
                     Tile tile = world.map[x, y];
 
                     if (tile.type != 0)
                     {
-                        if (y == posY+3)
-                        {
-                            inAir = false;
-                        }
-                        else{
-                            if (curSpeed.X > 0)
-                            {
-                                if (position.X + Settings.gridsize > x * Settings.gridsize)
-                                {
-                                    position.X--;
-                                }
-                            }
-                            else if (curSpeed.X < 0)
-                            {
 
-                            }
+                        if (position.X > posX * Settings.gridsize && position.X - texture.Width < posX * Settings.gridsize &&
+                            position.Y + texture.Height / 3 > posY * Settings.gridsize && curSpeed.X < 0)
+                        {
+                            position.X++;
+                            curSpeed.X = 0;
+                        }
+                        else if (position.X < posX * Settings.gridsize && position.X + texture.Width > posX * Settings.gridsize &&
+                            position.Y + texture.Height / 3 > posY * Settings.gridsize && curSpeed.X > 0)
+                        {
+                            position.X--;
+                            curSpeed.X = 0;
                         }
                     }
                 }
-            }
+            }*/
 
             if (!inAir)
             {
@@ -101,5 +128,5 @@ namespace Wizards
         }
 
     }
-        
+
 }
